6b96fc3ccb86d18d752b4d2aa63c4a3ec95ce1fd
olako <oldra_kovalenko@yahoo.de>
2022-02-12 18:41:46 +0100

    update DistilBert code to the latest version

--- DistilBert.py
+++ DistilBert.py
@@ -15,16 +15,16 @@ class DistilBert:
                                                          )
         return X_train, X_val, y_train, y_val
     
-    def bert_tokenize (self,X_train,X_val,X_test):
+    def bert_tokenize (self,X_train,X_val,X_test,max_len):
         print("Tokenizing")
         bert_tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
-        Xtrain_enc = bert_tokenizer(X_train.tolist(), max_length=256, #50 for sent140
+        Xtrain_enc = bert_tokenizer(X_train.tolist(), max_length=max_len,
                                     truncation=True, padding='max_length', 
                                     add_special_tokens=True, return_tensors='np')
-        Xval_enc = bert_tokenizer(X_val.tolist(), max_length=256,#50 for sent140 
+        Xval_enc = bert_tokenizer(X_val.tolist(), max_length=max_len,
                                   truncation=True, padding='max_length', 
                                   add_special_tokens=True, return_tensors='np')
-        Xtest_enc = bert_tokenizer(X_test.tolist(), max_length=256, #50 for sent140
+        Xtest_enc = bert_tokenizer(X_test.tolist(), max_length=max_len,
                                    truncation=True, padding='max_length', 
                                    add_special_tokens=True, return_tensors='np') #return numpy object
         print("Tokenizing completed")
@@ -39,8 +39,7 @@ class DistilBert:
                                                             y_test))
         return train_dataset, val_dataset, test_dataset
     
-    def bert_model(self,train_dataset,val_dataset,epochs):
-        max_len=256 #50 for sent140
+    def bert_model(self,train_dataset,val_dataset,epochs,max_len):
         epochs=self.epochs
         transformer = TFDistilBertModel.from_pretrained("distilbert-base-uncased")
         print("----Building the model----")
@@ -49,19 +48,18 @@ class DistilBert:
         sequence_output = transformer(input_ids,attention_mask)[0]
         cls_token = sequence_output[:, 0, :]
         x = Dense(512, activation='relu')(cls_token)
-        #x = Dropout(0.5)(x)
         y = Dense(1, activation='sigmoid')(x)
         model = Model(inputs=[input_ids,attention_mask], outputs=y)
         model.summary()
         model.compile(Adam(learning_rate=3e-5), loss='binary_crossentropy', metrics=['accuracy'])
         return model
     
-    def fit_distil_bert(self, sets,epochs):
+    def fit_distil_bert(self, sets,epochs,max_len):
         X_train, X_val, y_train, y_val = self.train_val_split(sets['X_train'],sets['y_train'])
-        Xtrain_enc,Xval_enc,Xtest_enc =self.bert_tokenize(X_train,X_val,sets['X_test'])
+        Xtrain_enc,Xval_enc,Xtest_enc =self.bert_tokenize(X_train,X_val,sets['X_test'],max_len)
         train_dataset, val_dataset, test_dataset =self.prepare_datasets(Xtrain_enc,Xval_enc,Xtest_enc,
                                                                         y_train,y_val,sets['y_test'])
-        model = self.bert_model(train_dataset,val_dataset,epochs=epochs)
+        model = self.bert_model(train_dataset,val_dataset,epochs=epochs,max_len=max_len)
         model.fit(train_dataset.batch(32),batch_size = 32,
                   epochs = epochs, validation_data = val_dataset.batch(32))
         print("Train score:", model.evaluate(train_dataset.batch(32)))
